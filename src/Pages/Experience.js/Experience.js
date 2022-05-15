fa65d7d0078751b40bf0d29937d49e688e69417c
Manny Singh <42702278+mannysinghh11@users.noreply.github.com>
2022-05-15 03:59:44 -0700

    added modal pop-up for experience cards

--- src/Pages/Experience.js/Experience.js
+++ src/Pages/Experience.js/Experience.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import ExperienceCard from "../../Components/ExperienceCard/ExperienceCard";
 
 import "./ExperienceStyle.css";
 import experienceInformation from "../../assets/json/experience.json";
 import educationInformation from "../../assets/json/education.json";
+import CustomModal from "../../Components/CustomModal/CustomModal";
 
-function handleWorkExperienceClicked() {
-    console.log("experience");
+function handleWorkExperienceClicked(positionTitle, location, description, setModalInformation, setModal) {
+    let informationObj = {
+        positionTitle: positionTitle,
+        location: location,
+        description: description
+    };
+
+    setModalInformation(informationObj);
+    setModal(true);
+}
+
+function handleScroll(e, modal) {
+    if(modal){
+        e.stopPropagation();
+    }
 }
 
 function Experience() {
+    const [modal, setModal] = useState(false);
+    const [modalInformation, setModalInformation] = useState();
+
     return (
-        <div className="mainContentContainer">
+        <div className="mainContentContainer" onWheel={(e) => handleScroll(e, modal)}>
             <div className="mainEducationContainer">
                 {educationInformation ? (
                     educationInformation.map((item, index) => {
@@ -38,22 +55,25 @@ function Experience() {
                 {experienceInformation ? (
                     experienceInformation.map((item, index) => {
                         const { cityState, dateEnd, dateStart, description, imageURL, location, type, positionTitle } = item;
+
                         return (
-                            <ExperienceCard
-                                key={index}
-                                cityState={cityState}
-                                dateStart={dateStart}
-                                dateEnd={dateEnd}
-                                description={description}
-                                image={imageURL}
-                                location={location}
-                                type={type}
-                                positionTitle={positionTitle}
-                            />
+                            <div key={index} onClick={() => handleWorkExperienceClicked(positionTitle, location, description, setModalInformation, setModal)}>
+                                <ExperienceCard
+                                    cityState={cityState}
+                                    dateStart={dateStart}
+                                    dateEnd={dateEnd}
+                                    image={imageURL}
+                                    location={location}
+                                    type={type}
+                                    positionTitle={positionTitle}
+                                />
+                            </div>
                         )
                     })
                 ) : <></>}
             </div>
+
+            <CustomModal isOpen={modal} setModal={setModal} modalInformation={modalInformation}/>
         </div>
     )
 }
